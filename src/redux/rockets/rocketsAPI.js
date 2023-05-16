6a93b93345a63bd3d5d4129793fa7295387eb9e1
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-15 19:39:41 -0600

    Remove try catch block

--- src/redux/rockets/rocketsAPI.js
+++ src/redux/rockets/rocketsAPI.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
 
-export const fetchRockets = async () => {
-  try {
-    const response = await axios.get('https://api.spacexdata.com/v3/rockets');
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching rockets', error);
-    throw error;
-  }
+const fetchRockets = async () => {
+  const response = await axios.get('https://api.spacexdata.com/v3/rockets');
+  return response.data;
 };
+
+export default fetchRockets;
