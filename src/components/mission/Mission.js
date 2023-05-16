79ebbbe29737f027421027764fcea1041a12cfe5
Booze33 <tebetisloh@gmail.com>
2023-05-16 05:23:36 -0500

    Display missions-List Render

--- src/components/mission/Mission.js
+++ src/components/mission/Mission.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchData } from '../../redux/mission/missionSlice';
+import './Mission.css';
 
 function Mission() {
   const dispatch = useDispatch();
@@ -17,12 +18,19 @@ function Mission() {
       {missions.status === 'loading' && <div>Loading...</div>}
       {missions.status === 'failed' && <div>{missions.error}</div>}
       {missions.status === 'succeeded' && (
-        <div>
+        <div className="big-container">
+          <div className="mission-container">
+            <h1 className="h1">Name</h1>
+            <h1 className="h1-description">Description</h1>
+            <h1 className="h1-status">Status</h1>
+          </div>
           {missions.data && missions.data.length > 0 ? (
             missions.data.map((mission) => (
-              <div key={mission.mission_id}>
-                <h2>{mission.mission_name}</h2>
-                <p>{mission.description}</p>
+              <div className="container" key={mission.mission_id}>
+                <h2 className="h2">{mission.mission_name}</h2>
+                <p className="description">{mission.description}</p>
+                <div className="mission-stats">Not A Member</div>
+                <button type="button" className="mission-btn">Join Mission </button>
               </div>
             ))
           ) : (
