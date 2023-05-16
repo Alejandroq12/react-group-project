739b7e7ea86494434dcc729b5d2ad97b72806676
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-15 20:00:26 -0600

    Add useDispatch, useSelector, and useEffect

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -2,3 +2,14 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchRocketsData } from '../../redux/rockets/rocketsSlice';
 
+const Rockets = () => {
+  const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.data);
+  const error = useSelector((state) => state.rockets.error);
+
+  useEffect(() => {
+    if (rockets.lenght === 0) {
+      dispatch(fetchRocketsData());
+    }
+  }, [dispatch, rockets]);
+};
