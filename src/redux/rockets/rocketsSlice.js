403576646e7d3526f3870896a450a1cc529fa629
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-15 20:30:48 -0600

    Add initial state

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,3 +1,8 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import fetchRockets from './rocketsAPI';
 
+const initialState = {
+  data: [],
+  status: 'idle',
+  error: null,
+};
