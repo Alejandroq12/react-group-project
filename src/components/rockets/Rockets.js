19d8e67e003b82fb0d8aef8c5e8ede94e9d896f2
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-05-16 00:26:31 -0600

    Add loading functionality and fallback font

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -7,6 +7,7 @@ const Rockets = () => {
   const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets.data);
   const error = useSelector((state) => state.rockets.error);
+  const loading = useSelector((state) => state.rockets.loading);
 
   useEffect(() => {
     if (rockets.length === 0) {
@@ -16,21 +17,23 @@ const Rockets = () => {
 
   return (
     <div className="rockets-container">
+      {loading && <div>Loading...</div>}
       {error && (
-      <div>
-        Error:
-        {' '}
-        {error}
-      </div>
+        <div>
+          Error:
+          {error}
+        </div>
       )}
       {rockets.map((rocket) => (
         <div key={rocket.id} className="rocket">
-          <img className="rocket-img" src={rocket.flickr_images[0]} alt={`${rocket.name}`} />
+          <img
+            className="rocket-img"
+            src={rocket.flickr_images[0]}
+            alt={`${rocket.name}`}
+          />
           <div className="rocket-info">
             <p className="rocket-name">{rocket.name}</p>
-            <p>
-              {rocket.description}
-            </p>
+            <p>{rocket.description}</p>
           </div>
         </div>
       ))}
